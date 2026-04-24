32b36c70b2b7cce7455ebdc8c4fc9389d0decb90
Ethan <ethankremerdojc@gmail.com>
2026-04-24 11:29:58 -0600

    oops

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('hexgame.urls')),
