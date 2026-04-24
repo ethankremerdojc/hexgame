90f9f42eb588c7a1368e357de630ef1869368fbc
Ethan <ethankremerdojc@gmail.com>
2026-04-24 11:44:46 -0600

    more settings updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,16 +105,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "hexgame/static/"),
-# ]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
